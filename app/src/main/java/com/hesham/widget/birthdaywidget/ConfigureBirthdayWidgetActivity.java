3e88746b42d1e1cbf4769a5534bcb9af823e5c44
HeshamFas <HeshamFas@live.com>
2015-08-14 18:29:19 -0700

    Continue working on the wedget, this commit contains errors

--- app/src/main/java/com/hesham/widget/birthdaywidget/ConfigureBirthdayWidgetActivity.java
+++ app/src/main/java/com/hesham/widget/birthdaywidget/ConfigureBirthdayWidgetActivity.java
@@ -1,6 +1,7 @@
 package com.hesham.widget.birthdaywidget;
 
 import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class ConfigureBirthdayWidgetActivity extends AppCompatActivity {
 
@@ -43,10 +47,20 @@ public class ConfigureBirthdayWidgetActivity extends AppCompatActivity {
     // finish the Activity
     private void saveConfiguration(View v){
         String name = this.getName();
-        String date = this.getdate();
+        String date = this.getDate();
         if(Utils.validateDate(date)== false){
-
+            setDate("wrong date: " + date);
+            return;
         }
+        if(this.mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID){
+            Toast.makeText(this, "Invalid Widget ID" ,Toast.LENGTH_LONG).show();
+            return;
+        }
+        updateAppWidgetLocal(name,date);
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        finish();
     }
 
     @Override
@@ -69,5 +83,41 @@ public class ConfigureBirthdayWidgetActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public String getName() {
+        EditText nameET = (EditText) findViewById(R.id.et_bdw_text_name);
+        return nameET.getText().toString();
+    }
+    private String getDate(){
+        EditText dateText = (EditText)findViewById(R.id.et_bdw_text_date);
+        return dateText.getText().toString();
+    }
+
+    private void setDate(String date){
+        EditText dateText = (EditText)findViewById(R.id.et_bdw_text_date);
+        dateText.setText(date);
+        dateText.requestFocus();
+    }
+    private void updateAppWidgetLocal( String name, String dob){
+        //Create an object to hold the data: widgetid, name and dob
+        BDayWidgetModel bdayWidgetModel = new BDayWidgetModel( mAppWidgetId,name,dob);
+        // create the view and send it to the home screen
+        updateAppWidget(this, AppWidgetManager.getInstance(this), bdayWidgetModel);
+        // use the data model object to save the id, name and dob in prefs
+        bdayWidgetModel.savePreferences(this);
+    }
+
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, BDayWidgetModel widgetModel){
+        //Construct a RemoteViews Object from the widget layout file
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.bday_widget);
+
+        //use the control ids in the layout to set values on them.
+        //notice that these methods are limited and available on the
+        //TextView directly to se these values.
+
+        remoteViews.setTextViewText(R.id.et_bdw_text_name, widgetModel.getName()+ ":" + "widgetModel.iid");
+
+
     }
 }
